4182f69113d0cc8d75c7fc09ae725e64927006c4
Adair <adairyves@gmail.com>
2023-12-03 18:51:16 +0100

    nuevos mensajes

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const comentariosDePersecucion = [
-    "¡Ey, cuidao! Hay uno detrás de ti, amigo.",
-    "¿Has visto a tu stalker número uno? ¡Qué creepy!",
-    "Bro, alguien te sigue, ¿no ves al detective privado ahí?",
-    "¿Estás en un juego de sigilo o te persiguen de verdad?",
-    "¡Amigo, alerta roja! Alguien va siguiéndote, ¡mucho ojo!",
-    "Si fuera tú, activaría el modo ninja, ¡hay un shadow en tu espalda!",
-    "¡Mirando tu espalda, bro! Alguien va con intenciones sospechosas.",
-    "Amigo, ¿quién es ese que te sigue? ¿Tu nuevo mejor amigo?",
-    "¿Estás grabando un thriller o hay un paparazzi en la ciudad?",
-    "¡Oye, parece que tu fan número uno está siguiendo tus pasos!",
-    "¡Ey, streamer! ¿Hoy toca streaming de persecución?",
-    "No quiero ser el aguafiestas, pero hay uno por ahí con cara de seguirte.",
-    "¡Amigo, hay algo raro! Alguien te sigue, ¿es un extra de película?",
-    "Hey, ¿alguien más ve a ese pato disfrazado de detective detrás de ti?",
-    "Ey, streamer, ¿te persigue alguien o solo has ganado un nuevo seguidor en la vida real?",
-    "¡OMG! ¿Has contratado a un asistente personal sin saberlo?",
-    "Hey, streamer, parece que tienes tu propio reality show callejero. ¡A disfrutar!",
-    "Maten a todos los gitanos"
+  "Primeerooo",
+  "Llevaba demasido tiempo esperando bro.",
+  "Por fin tenemos lo que nos merecemos",
+  "Saludas?",
+  "Que estudias?",
+  "Nunca te he imaginado siendo universitario",
+  "Bro, que precha",
+  "Primerooo",
+  "Os va bien? Se me congela la imagen",
+  "Habla mas alto, no se escucha",
+  "Te quierooo",
+  "Dale duro a la uni tio"
   ];
 
   var gamerNames = [
